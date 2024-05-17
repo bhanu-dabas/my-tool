82ae9ca8902e5ec1a6c99be5b8a576a170c8b7c9
bhanu-dabas <160500397+bhanu-dabas@users.noreply.github.com>
2024-05-18 01:41:19 +0530

    Update deployMongoDB.groovy

--- vars/deployMongoDB.groovy
+++ vars/deployMongoDB.groovy
@@ -8,10 +8,4 @@ def call() {
         inventory: 'dynamic_inventory_script.py',
         playbook: 'pb.yml'
     )
-    // Notification
-    emailext(
-        subject: 'Ansible Deployment Status',
-        body: 'The Ansible deployment has completed.',
-        to: 'bhanudabas7@gmail.com'
-    )
 }
